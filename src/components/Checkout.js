15c00e1e8ec1b6ef1632cde5af10dab50f3031b8
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2022-05-25 17:05:43 -0500

    styled and bug smashed

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,36 +2,66 @@ import React, { useState } from 'react'
 import PaypalCheckoutButton from './PaypalCheckoutButton'
 
 function Checkout() {
-  const [amount, setAmount] = useState('1.00')
+  const [product, setProduct] = useState({
+    description: '',
+    price: '',
+  })
 
-  const product = {
-    description: 'Friendly donation',
-    price: amount,
-  }
+  const [note, setNote] = useState('')
+  const [amount, setAmount] = useState()
 
-  const  handleAmountChange = (e) => {
-    //if value is not a number or 0, set to 20.00
-    if (isNaN(e.target.value) || e.target.value === '' || e.target.value == '0') {
+  const [ready, setReady] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (
+      amount === undefined ||
+      amount === '' ||
+      amount === null ||
+      amount == 0 ||
+      amount === NaN
+    ) {
+      setReady(false)
+      alert('Please enter a valid amount')
       setAmount('20.00')
     } else {
-      setAmount(e.target.value)
+      setReady(true)
+      setProduct({
+        description: note,
+        price: amount,
+      })
     }
   }
 
   return (
-    <div className="checkout-container">
-
-      <h1>FunAsFam.com Donation</h1>
-      <input
-        type="number"
-        placeholder="Please Donate"
-        onChange={handleAmountChange}
-        value={amount}
-      />
-      <p>Thank you!</p>
-      <div className="paypal-button-container">
-        <PaypalCheckoutButton product={product} />
-      </div>
+    <div>
+      {!ready ? (
+        <div className="checkout-container">
+          <h1>
+            <a className="domain">FunAsFam.com</a> Donation
+          </h1>
+          <input
+            className="amount-input"
+            type="number"
+            placeholder="💲💲💲"
+            onChange={(e) => setAmount(e.target.value)}
+            value={amount}
+          />
+          <input
+            className="note-input"
+            type="text"
+            placeholder="🥰 Note"
+            onChange={(e) => setNote(e.target.value)}
+            value={note}
+          />
+          <button onClick={handleSubmit}>Donate</button>
+          <p>Thank you!</p>
+        </div>
+      ) : (
+        <div className="paypal-button-container">
+          <PaypalCheckoutButton product={product} />
+        </div>
+      )}
     </div>
   )
 }
